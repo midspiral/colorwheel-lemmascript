a7918ba21a9f1fcd9d8082ae28104084999bf5f7
Nada Amin <namin@alum.mit.edu>
2026-04-05 18:37:03 -0400

    check extra

---
+++ check-extra.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Verify extra Dafny files not covered by lsc check.
+set -e
+cd "$(dirname "$0")"
+
+dafny verify src/colorwheel.proofs.dfy
+dafny verify src/colorwheel.props.dfy
+dafny verify src/colorwheel.sanity.dfy
